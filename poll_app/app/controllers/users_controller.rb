2fad711c3e935f6129dd3b55598bc5cb79653a09
堀之内 鈴乃 <23261082@kyoto-wu.ac.jp>
2025-11-18 03:52:29 +0000

    kanseisiro

--- poll_app/app/controllers/users_controller.rb
+++ poll_app/app/controllers/users_controller.rb
@@ -1,8 +1,10 @@
-# app/controllers/users_controller.rb
 class UsersController < ApplicationController
-  #skip_before_action :verify_authenticity_token, only: [:create]
+  # 認証済みユーザーのみが実行できるアクションの制限を、このコントローラーでは解除します。
+  # ログインや新規登録は、未認証ユーザーが行うためです。
+
   # GET /users/new (新規登録フォームの表示)
   def new
+    # 新しいユーザーオブジェクトを初期化し、フォームに渡します
     @user = User.new 
   end
   
@@ -11,13 +13,15 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     
     if @user.save
-      # 登録成功時の処理
+      # 登録成功時の処理: セッションにユーザーIDを保存し、ログイン状態にします
+      # 🚨 NOTE: `login(@user)`というヘルパーメソッドが定義されている場合は、
+      # 🚨 `session[:user_id] = @user.id` の代わりにそちらを使用することで、SessionsControllerと共通化できます。
       session[:user_id] = @user.id 
       flash[:success] = "登録が完了しました！"
       redirect_to root_path
     else
       # 登録失敗時 (バリデーションエラーなど) の処理
-      # render :new を使用しているか確認
+      # フォームの再表示とHTTPステータス422 (処理不能なエンティティ) を返します
       flash.now[:error] = "登録に失敗しました。"
       render :new, status: :unprocessable_entity
     end
@@ -25,8 +29,9 @@ class UsersController < ApplicationController
   
   private
   
+  # ストロングパラメーター
   def user_params
-    # パスワードの確認用フィールドを許可しているか確認
+    # 許可する属性: email, password, password_confirmation
     params.require(:user).permit(:email, :password, :password_confirmation)
   end
 end
--- poll_app/config/routes.rb
+++ poll_app/config/routes.rb
@@ -1,13 +1,13 @@
-# config/routes.rb
-
 Rails.application.routes.draw do
   # ホームと静的ページ
   root 'home#index'
   get '/help', to: 'home#help'
 
   # ユーザー登録 (新規作成)
+  # :new, :create のみを定義（show, edit, update, destroyは不要）
   resources :users, only: [:new, :create]
-  get '/register', to: 'users#new'
+  # 慣用的なパスとしてのエイリアス
+  get '/signup', to: 'users#new'
   
   # ログイン/ログアウト (SessionsController)
   get '/login', to: 'sessions#new'
@@ -18,11 +18,15 @@ Rails.application.routes.draw do
   resources :polls do
     member do
       # 投票処理 (POST /polls/:id/vote)
-      # これにより、poll_vote_path(@poll) が使えるようになります
+      # 投票を実行するアクション。ヘルパーメソッドは poll_vote_path(@poll)
       post 'vote', to: 'polls#create_vote', as: 'vote' 
       
       # 結果表示 (GET /polls/:id/result)
+      # 結果を確認するアクション。ヘルパーメソッドは result_poll_path(@poll) または poll_result_path(@poll)
       get 'result'
     end
   end
+  
+  # Health check route (Railsのデフォルト)
+  get "up" => "rails/health#show", as: :rails_health_check
 end
