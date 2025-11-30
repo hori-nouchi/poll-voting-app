1cc85431be8a3087640365bc21422b7459fbf0b9
堀之内 鈴乃 <23261082@kyoto-wu.ac.jp>
2025-11-30 16:15:51 +0000

    error fight4

--- poll_app/db/schema.rb
+++ poll_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_11_17_155006) do
+ActiveRecord::Schema[7.0].define(version: 2025_11_17_155006) do
   create_table "choices", force: :cascade do |t|
     t.string "content", null: false
     t.integer "poll_id", null: false
