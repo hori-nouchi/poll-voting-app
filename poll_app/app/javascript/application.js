88ce8067ed5cabb6de3822e4af70ef08508296aa
堀之内 鈴乃 <23261082@kyoto-wu.ac.jp>
2025-12-12 00:51:16 +0000

    1212 error syuusei 2

--- poll_app/app/javascript/application.js
+++ poll_app/app/javascript/application.js
@@ -1,6 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
-import ".controllers"
+import "./controllers"
 
 // Bootstrap 5のJavaScriptコンポーネントをロードするために必要
 import "@popperjs/core"
